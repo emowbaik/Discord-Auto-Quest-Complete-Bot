b213332c5fa96fa1402281f7df4474223fbd91e0
emowbaik <alamsantoso.12@gmail.com>
2026-08-18 17:35:06 +0800

    chore: add NoneCap extension v0.3.16_0 for hCaptcha auto-solve in CI/local

---
+++ extensions/nonecap/0.3.16_0/assets/tl-capture.ts-loader-B0oI-rD8.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  const injectTime = performance.now();
+  (async () => {
+    const { onExecute } = await import(
+      /* @vite-ignore */
+      "./tl-capture.ts-MpQQvI-z.js"
+    );
+    onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
+  })().catch(console.error);
+
+})();
